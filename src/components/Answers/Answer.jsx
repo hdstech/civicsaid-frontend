0a444903fb0e2755e10cebb5c555d24b13eb4067
MrBoutte101 <mrboutte21@gmail.com>
2017-08-13 20:07:51 -0400

    Toggle switch for hiding and showing the answer in flash card component

--- src/components/Answers/Answer.jsx
+++ src/components/Answers/Answer.jsx
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Collapse} from 'reactstrap';
+import Toggle from 'material-ui/Switch';
 import '../../styles/Main.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class Answer extends Component {
 	constructor(props) {
 		super(props);
+		this.toggleAnswer = this.toggleAnswer.bind(this);
 		this.state = {
 			answers: [],
 			languageSelected: '',
-		}
+			collapse: false,
+		};
 	}
 
 	componentWillMount() {
@@ -26,30 +30,41 @@ export default class Answer extends Component {
 		});
 	}
 
+	toggleAnswer() {
+		this.setState({collapse: !this.state.collapse});
+	}
+
 	render() {
 		let answers = this.state.answers;
 		let language = this.state.languageSelected;
 		return (
 			<div className="container-fluid">
-				<div className="card">
-					<div className="card-block">
-						{answers.map(answers =>
-							<div key={answers.id}>
-								<h4>
-									<li>
-										{
-											language === 'spanish' ? answers.a_spanish
-												: language === 'chinese' ? answers.a_chinese
-												: answers.a_english
-										}
-									</li>
-								</h4>
-							</div>
-						)}
-					</div>
+				<div className="card-header">
+					<h3>Answer<Toggle onClick={this.toggleAnswer}/></h3>
 				</div>
+				<Collapse isOpen={this.state.collapse}>
+					<div className="card">
+						<div className="card-block">
+							{answers.map(answers =>
+								<div key={answers.id}>
+									<h4>
+										<li>
+											{
+												language === 'spanish'
+													? answers.a_spanish
+													: language === 'chinese'
+													? answers.a_chinese
+													: answers.a_english
+											}
+										</li>
+									</h4>
+								</div>,
+							)}
+						</div>
+					</div>
+				</Collapse>
 			</div>
-		)
+		);
 	}
 }
 
