3a59d86ebe660a29d3a75c58a49f9962582c9f35
MrBoutte101 <mrboutte21@gmail.com>
2017-09-24 20:56:23 -0400

    Toggle Support for Hiding and Showing Answers

--- src/containers/Answers/index.js
+++ src/containers/Answers/index.js
@@ -1,29 +1,49 @@
 import React, {Component} from 'react';
-import '../../styles/Main.css';
-import { getAnswers } from '../../actions/answers';
+import Toggle from 'material-ui/Switch';
+import {Collapse} from 'reactstrap';
+import {getAnswers} from '../../actions/answers';
 import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux';
+import {bindActionCreators} from 'redux';
 import {LANGUAGE_CHINESE, LANGUAGE_SPANISH} from '../../actions/actionsTypes';
+import '../../styles/Main.css';
 
 class Answers extends Component {
+	constructor(props) {
+		super(props);
+		this.toggleAnswer = this.toggleAnswer.bind(this);
+		this.state = {
+			collapse: false
+		};
+	}
+
+	toggleAnswer() {
+		this.setState({collapse: !this.state.collapse});
+	}
+
 	render() {
 		const {answers, selectedLanguage} = this.props;
 		return (
 			<div className="container-fluid">
 				<div className="card">
-					<div className="card-body">
-						{answers.map((answer, i) => (
-							<div key={i}>
-								<li className="card-text-size">
-									{selectedLanguage === LANGUAGE_SPANISH
-									? answer.a_spanish
-									: selectedLanguage === LANGUAGE_CHINESE
-										? answer.a_chinese
-										: answer.a_english}
-								</li>
-							</div>
-						))}
+					<div className="card-header">
+						<h3>Show Answer<Toggle onClick={this.toggleAnswer}/></h3>
 					</div>
+					<Collapse isOpen={this.state.collapse}>
+						<div className="card-body">
+							{answers.map((answer, i) => (
+								<div key={i}>
+									<li className="card-text-size">
+										{selectedLanguage === LANGUAGE_SPANISH
+											? answer.a_spanish
+											: selectedLanguage ===
+											LANGUAGE_CHINESE
+												? answer.a_chinese
+												: answer.a_english}
+									</li>
+								</div>
+							))}
+						</div>
+					</Collapse>
 				</div>
 			</div>
 		);
@@ -34,12 +54,12 @@ function mapStateToProps(state) {
 	return {
 		answers: state.answers.answers,
 		question: state.question.selectedQuestion,
-		selectedLanguage: state.language.selectedLanguage
+		selectedLanguage: state.language.selectedLanguage,
 	};
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({getAnswers}, dispatch)
+	return bindActionCreators({getAnswers}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Answers);
