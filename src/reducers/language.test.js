a9046c0f39227806b4883703b206bfd6a8674178
MrBoutte101 <mrboutte21@gmail.com>
2017-10-14 16:37:44 -0400

    snapshot test for api coverage on fetching a single question

--- src/reducers/language.test.js
+++ src/reducers/language.test.js
@@ -10,21 +10,21 @@ test('should return the initial state', () => {
 	expect(language(undefined, {})).toEqual(initialState);
 });
 
-test('should be able to return the chinese as a selected language', () => {
+test('should be able to return chinese as a selected language', () => {
 	const expectedPayload = {
 		selectedLanguage: types.LANGUAGE_CHINESE
 	};
 	expect(language({}, actionCreators.languageSelected(types.LANGUAGE_CHINESE))).toEqual(expectedPayload);
 });
 
-test('should be able to return the spanish as a selected language', () => {
+test('should be able to return spanish as a selected language', () => {
 	const expectedPayload = {
 		selectedLanguage: types.LANGUAGE_SPANISH
 	};
 	expect(language({}, actionCreators.languageSelected(types.LANGUAGE_SPANISH))).toEqual(expectedPayload);
 });
 
-test('should be able to return the english as a selected language', () => {
+test('should be able to return english as a selected language', () => {
 	const expectedPayload = {
 		selectedLanguage: types.LANGUAGE_ENGLISH
 	};
---
+++ src/services/question.test.js
@@ -0,0 +1,11 @@
+import "isomorphic-fetch";
+
+test('should return a single question object', async () => {
+	try {
+		const response = await fetch(`http://civicsaid-backend.dev/questions/show-question/6`);
+		const fetchedQuestion = await response.json();
+		expect(fetchedQuestion).toMatchSnapshot();
+	} catch (e){
+		fail();
+	}
+});
