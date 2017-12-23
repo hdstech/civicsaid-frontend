4cd939b3cda99f7c270011b873fbe7520860ba7a
MrBoutte101 <mrboutte21@gmail.com>
2017-12-23 13:05:24 -0500

    Header.jsx WIP

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,18 +30,18 @@ export default class Header extends Component {
                         <DropdownToggle nav caret className="white-link">Questions</DropdownToggle>
                         <DropdownMenu className="drop-down">
                             <DropdownItem>
-                                <Link to="/all-questions" className="white-link">All Questions</Link>
+                                <Link to="/questions/all-questions" className="white-link">All Questions</Link>
                             </DropdownItem>
                             <DropdownItem divider/>
                             <DropdownItem header><em><strong className="muted-txt">Categories</strong></em></DropdownItem>
                             <DropdownItem>
-                                <Link to={`/category/american-government`} className="white-link">American Government</Link>
+                                <Link to={`/questions/american-government`} className="white-link">American Government</Link>
                             </DropdownItem>
                             <DropdownItem>
-                                <Link to={`/category/american-history`} className="white-link">American History</Link>
+                                <Link to={`/questions/american-history`} className="white-link">American History</Link>
                             </DropdownItem>
                             <DropdownItem>
-                                <Link to={`/category/integrated-civics`} className="white-link">Integrated Civics</Link>
+                                <Link to={`/questions/integrated-civics`} className="white-link">Integrated Civics</Link>
                             </DropdownItem>
                         </DropdownMenu>
                     </NavDropdown>
